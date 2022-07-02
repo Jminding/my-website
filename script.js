507886b75b9f457ed097dde31cd80eb28dc14204
Jaymin Ding <jtding43@gmail.com>
2022-07-02 00:07:41 -0400

    Attempting to make form submission work 2

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
-var frmvalidator  = new Validator("contactform");
-frmvalidator.addValidation("name", "req", "Please provide your name");
-frmvalidator.addValidation("email", "req", "Please provide your email");
-frmvalidator.addValidation("email","email", "Please enter a valid email address");
+// var frmvalidator  = new Validator("contactform");
+// frmvalidator.addValidation("name", "req", "Please provide your name");
+// frmvalidator.addValidation("email", "req", "Please provide your email");
+// frmvalidator.addValidation("email","email", "Please enter a valid email address");
